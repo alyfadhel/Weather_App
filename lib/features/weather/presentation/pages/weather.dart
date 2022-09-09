74183447193289a7db21702077338e9a613c2626
alyfa <alyfadel67@gmail.com>
2022-09-09 15:57:48 +0200

    update forecast

--- lib/features/weather/presentation/pages/weather.dart
+++ lib/features/weather/presentation/pages/weather.dart
@@ -542,7 +542,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                   listModel.weather![0].description.toString(),
                   style: TextStyle(
                     color: Colors.white.withOpacity(.5),
-                    fontSize: 12.8,
+                    fontSize: 12.0,
                   ),
                 ),
               ],
